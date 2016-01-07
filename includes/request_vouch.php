827c8f6cb0d8600ca6221ba63b51a856a9d4e238
David Jetelina <djt@seznam.cz>
2016-01-07 14:45:16 +0100

    pattern fix

--- includes/request_vouch.php
+++ includes/request_vouch.php
@@ -3,7 +3,7 @@
 <div id="vouchingform">
     <form action="http://api.xleague.info/reqvouch" method="post" id="vouchform">
         <p>Username:</p>
-        <input type="text" name="name" placeholder="GameSurge nickname" patter="[a-zA-Z0-9]+" required>
+        <input type="text" name="name" placeholder="GameSurge nickname" pattern="[a-zA-Z0-9]+" required>
         <p>About yourself:</p>
         <textarea name="about" placeholder="Introduce yourself, tell us why are you interested and why we shouldn't hesitate to accept you!" rows="4" required></textarea>
         <div class="g-recaptcha" id="rcaptcha" data-sitekey="6Lc4ixQTAAAAAKQCUEyky-ndknJkZH2welXMOIXS" data-theme="dark"></div>
